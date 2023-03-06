0d2c798d1f3ce40badf6d4c96733dc0943e5c4a4
AfonyaGit <nikitoz110@gmail.com>
2023-03-06 22:43:51 +0300

    path upgrade
    add normal generation(without exhibiting tiles)

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
 	private PathGenerator pathGenerator;
+
 	public int gridWidth = 16;
 	public int gridHeight = 8;
-	public int minPathLength=30;
-	public GameObject pathTile;
+	public int minPathLength = 30;
+
+	public GridCellObject[] gridCells;
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -28,7 +31,12 @@ public class GridManager : MonoBehaviour
 	{
 		foreach (Vector2Int pathCell in pathCells)
 		{
-			Instantiate(pathTile, new Vector3(pathCell.x, 0, pathCell.y), Quaternion.identity);
+			int neighbourValue = pathGenerator.getCellNeighbourValue(pathCell.x, pathCell.y);
+
+			Debug.Log("Tile" + pathCell.x + "," + pathCell.y + "neighbour=" + neighbourValue);
+			GameObject pathTile = gridCells[neighbourValue].cellPrefab;
+			GameObject pathTileCell= Instantiate(pathTile, new Vector3(pathCell.x, 0, pathCell.y), Quaternion.identity);
+			
 			yield return new WaitForSeconds(0.3f);
 		}
 		yield return null;
--- Assets/Scripts/PathGenerator.cs
+++ Assets/Scripts/PathGenerator.cs
@@ -37,12 +37,12 @@ public class PathGenerator
 					x++;
 					validMove = true;
 				}
-				else if (move == 1 && CellIsFree(x, y + 1) && y < (height - 3))
+				else if (move == 1 && CellIsEmpty(x, y + 1) && y < (height - 2))
 				{
 					y++;
 					validMove = true;
 				}
-				else if (move == 2 && CellIsFree(x, y - 1) && y > 2)
+				else if (move == 2 && CellIsEmpty(x, y - 1) && y > 2)
 				{
 					y--;
 					validMove = true;
@@ -51,8 +51,38 @@ public class PathGenerator
 		}
 		return pathCells;
 	}
-	private bool CellIsFree(int x, int y)
+	private bool CellIsEmpty(int x, int y)
 	{
 		return !pathCells.Contains(new Vector2Int(x, y));
 	}
+
+	private bool CellIsTaken(int x, int y)
+	{
+		return pathCells.Contains(new Vector2Int(x, y));
+	}
+	
+	public int getCellNeighbourValue(int x, int y)
+	{
+		int returnValue = 0;
+
+		if (!CellIsTaken(x, y - 1))//bottom
+		{
+			returnValue += 1;
+		}
+		if (!CellIsTaken(x - 1, y))//left
+		{
+			returnValue += 2;
+		}
+		if (!CellIsTaken(x + 1, y))//right
+		{
+			returnValue += 4;
+		}
+		if (!CellIsTaken(x, y + 1))//top
+		{
+			returnValue += 8;
+		}
+		return returnValue;
+	}
+
+	
 }
